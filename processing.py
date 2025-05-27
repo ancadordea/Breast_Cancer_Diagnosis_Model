29fe20350b2421becba18fae614394f4af276164
Anca Dordea <124100634+ancadordea@users.noreply.github.com>
2025-05-27 11:34:36 +0100

    Add files via upload

---
+++ processing.py
@@ -0,0 +1,95 @@
+import numpy as np
+import pandas as pd
+from sklearn.model_selection import train_test_split
+
+# Load dataset
+data = pd.read_csv("breastcancer_data.csv")
+
+# Data processing
+data.drop(['Unnamed: 32', 'id'], axis=1, inplace=True)
+data['diagnosis'] = data['diagnosis'].map({'M': 1, 'B': 0})
+y = data['diagnosis'].values
+x_data = data.drop(['diagnosis'], axis=1)
+
+# Data normalisation
+x = (x_data - x_data.min()) / (x_data.max() - x_data.min())
+
+# Train and test
+x_train, x_test, y_train, y_test = train_test_split(x, y, test_size = 0.15, random_state = 42)
+
+x_train = x_train.T
+x_test = x_test.T
+y_train = y_train.T
+y_test = y_test.T
+
+print("x train: ", x_train.shape)
+print("x test: ", x_test.shape)
+print("y train: ", y_train.shape)
+print("y test: ", y_test.shape)
+
+# Initiatising model
+# Weight and bias
+def initialize_weights_and_bias(dimension):
+    w = np.random.randn(dimension, 1) * 0.01  
+    b = 0.0
+    return w, b
+
+# Sigmoid function to calculate z
+def sigmoid(z):
+    return 1 / (1 + np.exp(-z))
+
+# Propagation 
+def forward_backward_propagation(w, b, x_train, y_train):
+    m = x_train.shape[1]
+    z = np.dot(w.T, x_train) + b
+    y_head = sigmoid(z)
+    
+   
+    cost = (-1/m) * np.sum(y_train * np.log(y_head) + (1 - y_train) * np.log(1 - y_head))
+    
+    derivative_weight = (1/m) * np.dot(x_train, (y_head - y_train).T)
+    derivative_bias = (1/m) * np.sum(y_head - y_train)
+    
+    gradients = {"derivative_weight": derivative_weight, "derivative_bias": derivative_bias}
+    return cost, gradients
+
+# Parameters
+def update(w, b, x_train, y_train, learning_rate, num_iterations):
+    costs = []
+    gradients = {}
+    for i in range(num_iterations):
+        cost, grad = forward_backward_propagation(w, b, x_train, y_train)
+        w -= learning_rate * grad["derivative_weight"]
+        b -= learning_rate * grad["derivative_bias"]
+
+        if i % 100 == 0:
+            costs.append(cost)
+            print(f"Cost after iteration {i}: {cost}")
+
+    parameters = {"weight": w, "bias": b}
+    return parameters, gradients, costs
+
+# Model predictions
+def predict(w, b, x_test):
+    m = x_test.shape[1]
+    y_prediction = np.zeros((1, m))
+    z = sigmoid(np.dot(w.T, x_test) + b)
+
+    for i in range(z.shape[1]):
+        y_prediction[0, i] = 1 if z[0, i] > 0.5 else 0
+
+    return y_prediction
+
+# Logistic regression
+def logistic_regression(x_train, y_train, x_test, y_test, learning_rate=0.01, num_iterations=1000):
+    dimension = x_train.shape[0]
+    w, b = initialize_weights_and_bias(dimension)
+    parameters, gradients, costs = update(w, b, x_train, y_train, learning_rate, num_iterations)
+    
+    y_prediction_test = predict(parameters["weight"], parameters["bias"], x_test)
+    y_prediction_train = predict(parameters["weight"], parameters["bias"], x_train)
+    
+    print(f"Train accuracy: {100 - np.mean(np.abs(y_prediction_train - y_train)) * 100}%")
+    print(f"Test accuracy: {100 - np.mean(np.abs(y_prediction_test - y_test)) * 100}%")
+
+logistic_regression(x_train, y_train, x_test, y_test, learning_rate=0.01, num_iterations=1000)
